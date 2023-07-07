d68d7bbc1a972a08e2c54c8101abf29683e0da02
sendoxmedia <sendoxmedia@gmail.com>
2023-07-07 16:22:48 -0700

    1. Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,22 +1,11 @@
 #include "main.h"
+
 /**
- *_memcpy - a function that copies memory area
- *@dest: memory where is stored
- *@src: memory where is copied
- *@n: number of bytes
- *
- *Return: copied memory with n byted changed
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+int _isdigit(int c)
 {
-	int r = 0;
-	int i = n;
-
-
-	for (; r < i; r++)
-	{
-		dest[r] = src[r];
-		n--;
-	}
-	return (dest);
+	return (c >= '0' && c <= '9');
 }
