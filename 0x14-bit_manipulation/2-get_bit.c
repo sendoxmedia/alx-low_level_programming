ea3103dbf9fea60436e89280e29fa44b4963e1ed
sendoxmedia <sendoxmedia@gmail.com>
2023-08-03 14:56:55 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_bit - function that returns the value of a bit at an index in a decimal number
+ * get_bit - this returns the value of a bit at an index in a decimal number
  * @n: number to search
  * @index: index of the bit
  *
